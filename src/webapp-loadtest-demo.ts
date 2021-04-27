09b17e8ca83deda4d5123996ceac454753114f10
Donggeun Lee <piou987@gmail.com>
2021-04-27 11:52:45 +0900

    Modify Application Class Structure

--- src/webapp-loadtest-demo.ts
+++ src/webapp-loadtest-demo.ts
@@ -1,49 +1,60 @@
 import { Chart, ChartProps } from "cdk8s";
 import { Construct } from "constructs";
-import { KubeDeployment, KubeHorizontalPodAutoscaler } from "../imports/k8s";
+import { KubeDeployment, KubeHorizontalPodAutoscaler, KubeService } from "../imports/k8s";
 
 export class Application {
   constructor(scope: Construct, id: string, props: ChartProps = { }) {
-    new DeploymentChart(scope, `${id}-deployment`, props)
-    new HpaChart(scope, `${id}-hpa`, props)
+    const info = {
+      image: '242593025403.dkr.ecr.us-east-1.amazonaws.com/webapp-loadtest-demo:0.0.4-SNAPSHOT',
+      label: {
+        'app.kubernetes.io/name': 'webapp-loadtest-demo',
+        'app.kubernetes.io/version': '0.0.4-SNAPSHOT'
+      },
+      limit: {
+        cpu: '1000m',
+        memory: '2000Mi'
+      },
+      port: 8080,
+      name: 'webapp-loadtest-demo'
+    }
+    
+    new DeploymentChart(scope, `${id}-deployment`, props, info)
+    new HpaChart(scope, `${id}-hpa`, props, info)
+    new ServiceChart(scope, `${id}-service`, props, info)
   }
 }
 
 class DeploymentChart extends Chart {
-  constructor(scope: Construct, id: string, props: ChartProps = { }) {
+  constructor(scope: Construct, id: string, props: ChartProps = { }, info: any) {
     super(scope, id, props);
 
     // write code here
-    const label = {
-      'app.kubernetes.io/name': 'webapp-loadtest-demo',
-      'app.kubernetes.io/version': '0.0.4-SNAPSHOT'
-    }
 
     new KubeDeployment(this, 'webapp-loadtest-demo', {
       spec: {
         replicas: 2,
         selector: {
-          matchLabels: label
+          matchLabels: info.label
         },
         template: {
           metadata: {
-            labels: label
+            labels: info.label
           },
           spec: {
             terminationGracePeriodSeconds: 60,
             containers: [
               {
-                name: 'webapp-loadtest-demo',
-                image: '242593025403.dkr.ecr.us-east-1.amazonaws.com/webapp-loadtest-demo:0.0.4-SNAPSHOT',
-                ports: [ { containerPort: 8080 }],
+                name: info.name,
+                image: info.image,
+                ports: [ { containerPort: info.port }],
                 resources: {
                   requests: {
-                    cpu: '1000m',
-                    memory: '2000Mi'
+                    cpu: info.limit.cpu,
+                    memory: info.limit.memory
                   },
                   limits: {
-                    cpu: '1000m',
-                    memory: '2000Mi'
+                    cpu: info.limit.cpu,
+                    memory: info.limit.memory
                   }
                 },
                 readinessProbe: {
@@ -78,7 +89,7 @@ class DeploymentChart extends Chart {
 }
 
 class HpaChart extends Chart {
-  constructor(scope: Construct, id: string, props: ChartProps = { }) {
+  constructor(scope: Construct, id: string, props: ChartProps = { }, info: any) {
     super(scope, id, props);
 
     // write code here
@@ -87,7 +98,7 @@ class HpaChart extends Chart {
         scaleTargetRef: {
           apiVersion: 'apps/v1',
           kind: 'Deployment',
-          name: 'webapp-loadtest-demo'
+          name: info.name
         },
         minReplicas: 2,
         maxReplicas: 20,
@@ -111,6 +122,26 @@ class HpaChart extends Chart {
                 averageValue: '1000Mi'
               }
             }
+          }
+        ]
+      }
+    })
+  }
+}
+
+class ServiceChart extends Chart {
+  constructor(scope: Construct, id: string, props: ChartProps = { }, info: any) {
+    super(scope, id, props);
+
+    // write code here
+    new KubeService(this, 'webapp-loadtest-demo-service', {
+      spec: {
+        selector: info.label,
+        type: 'ClusterIP',
+        ports: [
+          {
+            port: info.port,
+            targetPort: info.port
           }
         ]
       }
