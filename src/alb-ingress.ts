4583e63e90fd9769d4276d53cecd22de5167b641
Donggeun Lee <piou987@gmail.com>
2021-04-29 09:40:38 +0900

    Modify alb-ingress annotations

--- src/alb-ingress.ts
+++ src/alb-ingress.ts
@@ -11,9 +11,20 @@ export class AlbIngressChart extends Chart {
         annotations: {
           'kubernetes.io/ingress.class': 'alb',
           'alb.ingress.kubernetes.io/scheme': 'internet-facing',
-          'alb.ingress.kubernetes.io/listen-ports': '[{"HTTP":80}]',
+          'alb.ingress.kubernetes.io/listen-ports': '[{"HTTPS":443}]',
+          'alb.ingress.kubernetes.io/certificate-arn': 'arn:aws:acm:us-west-2:242593025403:certificate/99e510d8-fe48-423a-81e2-d647dab91235',
+          'alb.ingress.kubernetes.io/ssl-policy': 'ELBSecurityPolicy-2016-08',
+          'alb.ingress.kubernetes.io/security-groups': 'sg-0bcaa7d07a79af115',
           'alb.ingress.kubernetes.io/target-type': 'ip',
-          'alb.ingress.kubernetes.io/healthcheck-path': '/healthcheck'
+          'alb.ingress.kubernetes.io/backend-protocol': 'HTTP',
+          'alb.ingress.kubernetes.io/healthcheck-path': '/healthcheck',
+          'alb.ingress.kubernetes.io/healthcheck-interval-seconds': '15',
+          'alb.ingress.kubernetes.io/healthcheck-timeout-seconds': '5',
+          'alb.ingress.kubernetes.io/healthy-threshold-count': '2',
+          'alb.ingress.kubernetes.io/unhealthy-threshold-count': '2',
+          'alb.ingress.kubernetes.io/target-group-attributes': 'slow_start.duration_seconds=30,deregistration_delay.timeout_seconds=60',
+          'external-dns.alpha.kubernetes.io/ttl': '60',
+          'external-dns.alpha.kubernetes.io/hostname': 'sample.skill53.cloud'
         }
       },
       spec: {
